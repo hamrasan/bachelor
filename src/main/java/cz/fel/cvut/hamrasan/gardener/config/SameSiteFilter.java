8e4430ae87fff7dd6995d3b88f0b7b9ec89c0603
Sandra Hamráková <hamrasan@fel.cvut.cz>
2021-05-20 21:07:49 +0200

    Same site filter

--- src/main/java/cz/fel/cvut/hamrasan/gardener/config/SameSiteFilter.java
+++ src/main/java/cz/fel/cvut/hamrasan/gardener/config/SameSiteFilter.java
@@ -1,5 +1,6 @@
 package cz.fel.cvut.hamrasan.gardener.config;
 
+import cz.fel.cvut.hamrasan.gardener.service.security.LoginService;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -8,12 +9,16 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class SameSiteFilter extends GenericFilterBean {
+    private final static Logger LOGGER = Logger.getLogger(SameSiteFilter.class.getName());
+
     @Override
     public void doFilter (ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse resp = (HttpServletResponse) response;
-        resp.setHeader("Set-Cookie", "SameSite=None; Secure");
+        LOGGER.info(resp.getHeader("Set-Cookie"));
+        resp.setHeader("Set-Cookie", "SameSite=None; Secure;" + resp.getHeader("Set-Cookie"));
         chain.doFilter(request, response);
     }
 
